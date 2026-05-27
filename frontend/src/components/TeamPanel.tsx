9c94ccea9abd6ced17d2fb17d6f0ea499e13c19f
Onegai <onegai0@users.noreply.github.com>
2026-05-27 17:41:13 -0300

    feat: basic test UI with team panel and dashboard

---
+++ frontend/src/components/TeamPanel.tsx
@@ -0,0 +1,20 @@
+
+
+type TeamPanelProps = {
+    type: string,
+};  
+
+export function TeamPanel({ type }: TeamPanelProps) {
+    return (
+        <div className="w-full h-full bg-[#1E1E1E] rounded-lg p-4">
+            <h2 className="text-xl font-bold mb-4   flex justify-center"> {type}</h2>
+
+            {[1, 2, 3].map((item) => (
+                <div key={item} className="w-full h-16 bg-[#2A2A2A] rounded mb-3 flex items-center px-4">
+                    <div className="w-10 h-10 bg-gray-500 rounded-full mr-4"></div>
+                    <div className="text-white">Atendente {item}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,24 +5,62 @@ import { attendantService } from '../services/attendanceService';
 import { useSignalR } from '../hooks/useSignalR';
 import type { IAttendant } from '../interfaces/IAttendant';
 import type { IAttendance } from '../interfaces/IAttendance';
+import { AttendanceStatus, AttendanceStatusNames, AttendanceTeam, AttendanceTeamNames } from "../interfaces/Enum";
+import { TeamPanel } from '../components/TeamPanel';
 
 export function Dashboard() {
 
 
-const [attendances, setAttendances] = useState<IAttendance[]>([]);
-const [attendants, setAttendants] = useState<IAttendant[]>([]);
+    const [attendances, setAttendances] = useState<IAttendance[]>([]);
+    const [attendants, setAttendants] = useState<IAttendant[]>([]);
 
-const fetchData = useCallback(() => {
-    attendanceService.getAll().then(setAttendances);
-    attendantService.getAll().then(setAttendants);
-}, []);
+    const fetchData = useCallback(() => {
+        attendanceService.getAll().then(setAttendances);
+        attendantService.getAll().then(setAttendants);
+    }, []);
 
-useSignalR(fetchData);
-console.log(attendances)
-console.log(attendants)
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+    useSignalR(fetchData);
+    const buttonClass =
+        "mt-5 px-4 py-2 bg-gray-800 text-white rounded hover:bg-blue-600";
+
+    const wrapType = "flex flex-wrap justify-center gap-5 pt-5 px-5";
+    const teamKeys = Object.values(AttendanceTeam) as AttendanceTeam[];
+    const statusKeys = Object.values(AttendanceStatus) as AttendanceStatus[];
+    return (
+        <div className=" flex flex-col  items-center w-full h-full">
+            <div className={wrapType}>
+                {statusKeys.map((status) => (
+                    <div key={status} className='flex-1 min-w-[350px] max-w-[600px]'>
+                        <TeamPanel type={AttendanceStatusNames[status]} />
+                    </div>
+                ))}
+            </div>
+
+            <div>
+
+                <div className={wrapType}>
+                    {teamKeys.map((team) => (
+                        <div key={team} className='flex-1 min-w-[350px] max-w-[600px]'>
+                            <TeamPanel type={AttendanceTeamNames[team]} />
+                        </div>
+                    ))}
+                </div>
+
+                <div className=" flex justify-end px-5 gap-5">
+                    <button
+                        onClick={fetchData}
+                        className={buttonClass}
+                    >
+                        Novo Atendente
+                    </button>
+                    <button
+                        onClick={fetchData}
+                        className={buttonClass}
+                    >
+                        Novo Atendimento
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
